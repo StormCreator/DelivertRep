dd4aa2c278c9b4ccf3fde1d6db47923c8056bd77
Dima <voronoydima@gmail.com>
2020-06-02 20:54:42 +0300

    add hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -57,3 +57,21 @@ $(document).ready(function(){
         return false;
     });
 });
+// navigation
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
